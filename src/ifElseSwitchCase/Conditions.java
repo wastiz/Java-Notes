2762761fa8ecbfa45f6cff68ae5812af8d45bc6f
Valeri Nossov <valnos04@gmail.com>
2024-02-22 16:06:21 +0200

    first commit

---
+++ src/ifElseSwitchCase/Conditions.java
@@ -0,0 +1,6 @@
+package ifElseSwitchCase;
+
+public class Conditions {
+    // Скажу сразу. Абсолютно такой же синтаксис как в js. Вот полностью, что с if-else что с switch-case.
+    // Поэтому пока здесь писать не буду про это
+}
